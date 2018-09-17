0869a819b80f8779f9478f216e41f24697fd31bc
phengemuhle <phengemuhle@gmail.com>
2018-09-17 15:59:29 -0600

    added code and finished product

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ window.onpageshow = function () {
     var img = document.getElementById("gradientid");
     ctx.drawImage(img, 0, 0, 300, 150);
 }
-var img = document.querySelector('canvas')
-img.addEventListener('click', function (event) {
-    var context = img.getContext('2d');
+// var img = document.querySelector('canvas')
+// img.addEventListener('click', function (event) {
+//     var context = img.getContext('2d');
 
-    console.log(context)
-    console.dir(event.target)
+var canvas = document.getElementById("gradientid");
+canvas.addEventListener("click", function (event) {
+    // Get the coordinates of the click
+    var eventLocation = getEventLocation(this, event);
+    // Get the data of the pixel according to the location generate by the getEventLocation function
+    var context = this.getContext('2d');
+    var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data;
+console.log(pixelData)
+    // If transparency on the pixel , array = [0,0,0,0]
+    if ((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)) {
+        // Do something if the pixel is transparent
+    }
+    
+    // Convert it to HEX if you want using the rgbToHex method.
+    // var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
+}, false);
 
-});
 
---
+++ pixelartfinal/app.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function () {
+    for (let i = 1; i < 2117; i++) {
+        var box1 = document.createElement('main');
+        var container = document.querySelector('div1');
+        container.appendChild(box1);
+        box1.setAttribute('class', "box");
+
+    }
+    var color1 = document.querySelector('.color1')
+    var color2 = document.querySelector('.color2')
+    var color3 = document.querySelector('.color3')
+    var color4 = document.querySelector('.color4')
+    var color5 = document.querySelector('.color5')
+    var color6 = document.querySelector('.color6')
+    var color7 = document.querySelector('.color7')
+    var color8 = document.querySelector('.color8')
+    var brush = document.querySelector('.colorChoice')
+    var colorsplash = document.querySelector('.fillBox')
+    var boxes = document.querySelectorAll('main')
+    var clear = document.querySelector('.clear')
+   
+
+    color1.addEventListener("click", function(event){
+        brush.style.backgroundColor = "red"
+    })
+    color2.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "blue"
+    })
+    color3.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "yellow"
+    })
+    color4.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "orange"
+    })
+    color5.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "purple"
+    })
+    color6.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "green"
+    })
+    color7.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "pink"
+    })
+    color8.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "black"
+    })
+    
+    colorsplash.addEventListener("click", function (event) {
+        for (let i = 0; i < container.children.length; i++){
+            container.children[i].style.backgroundColor = brush.style.backgroundColor
+        }
+    })
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('click', function (event) {
+            event.target.style.backgroundColor = brush.style.backgroundColor
+        })
+    }
+    clear.addEventListener("click", function (event) {
+        for (let i = 0; i < container.children.length; i++) {
+            container.children[i].style.backgroundColor = 'white'
+        }
+    })
+});
