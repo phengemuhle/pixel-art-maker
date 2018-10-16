8d24dca14b7666fa0dfc8a9c792efc922eb69acd
phengemuhle <phengemuhle@gmail.com>
2018-10-16 17:56:54 -0600

    fixed box size and code

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
-document.addEventListener('DOMContentLoaded', function(){
-    for(let i =1; i<2100; i++){
-       var box1 = document.createElement('main');
-        var container = document.querySelector('div');
+document.addEventListener("DOMContentLoaded", function () {
+    for (let i = 1; i < 980; i++) {
+        var box1 = document.createElement("main");
+        var container = document.querySelector("div1");
         container.appendChild(box1);
-        box1.setAttribute('class', "box");
+        box1.setAttribute("class", "box");
+    }
+    var red = document.querySelector(".red")
+    var blue = document.querySelector(".blue")
+    var yellow = document.querySelector(".yellow")
+    var orange = document.querySelector(".orange")
+    var purple = document.querySelector(".purple")
+    var green = document.querySelector(".green")
+    var pink = document.querySelector(".pink")
+    var black = document.querySelector(".black")
+    var brush = document.querySelector(".colorChoice")
+    var colorsplash = document.querySelector(".fillBox")
+    var boxes = document.querySelectorAll("main")
+    var clear = document.querySelector(".clear")
 
+
+    red.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "red"
+    })
+    blue.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "blue"
+    })
+    yellow.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "yellow"
+    })
+    orange.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "orange"
+    })
+    purple.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "purple"
+    })
+    green.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "green"
+    })
+    pink.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "pink"
+    })
+    black.addEventListener("click", function (event) {
+        brush.style.backgroundColor = "black"
+    })
+
+    colorsplash.addEventListener("click", colorFill)
+    clear.addEventListener("click", removeColor)
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("click", function (event) {
+            if (event.target.style.backgroundColor === brush.style.backgroundColor) {
+                event.target.style.backgroundColor = "white"
+            } else {
+                event.target.style.backgroundColor = brush.style.backgroundColor
+            }
+        })
+    }
+    function colorFill(event) {
+        for (let i = 0; i < container.children.length; i++) {
+            container.children[i].style.backgroundColor = brush.style.backgroundColor
+        }
+    }
+    function removeColor(event) {
+        for (let i = 0; i < container.children.length; i++) {
+            container.children[i].style.backgroundColor = "white"
+        }
     }
 });
-
-
-window.onpageshow = function () {
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("gradientid");
-    ctx.drawImage(img, 0, 0, 300, 150);
-}
-// var img = document.querySelector('canvas')
-// img.addEventListener('click', function (event) {
-//     var context = img.getContext('2d');
-
-var canvas = document.getElementById("gradientid");
-canvas.addEventListener("click", function (event) {
-    // Get the coordinates of the click
-    var eventLocation = getEventLocation(this, event);
-    // Get the data of the pixel according to the location generate by the getEventLocation function
-    var context = this.getContext('2d');
-    var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data;
-console.log(pixelData)
-    // If transparency on the pixel , array = [0,0,0,0]
-    if ((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)) {
-        // Do something if the pixel is transparent
-    }
-    
-    // Convert it to HEX if you want using the rgbToHex method.
-    // var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
-}, false);
-
-
